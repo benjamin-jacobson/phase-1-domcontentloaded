9578646823ec64f265b84b5284145c10b333ab83
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-05-26 20:42:15 -0400

    compelted lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let x = document.querySelector('#text')
+    console.log(x.textContent)
+    x.textContent = 'This is really cool!'
+    
+  });
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  console.log(
+    "Eat my shorts lol"
+  );
